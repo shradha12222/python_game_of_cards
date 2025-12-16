5b7b080f5d41c944e161b07be411cfb93d7e47e3
sk-pro <shradhakanjariya@gmail.com>
2025-12-16 10:20:50 +0530

    Add ASCII art logo to art.py

---
+++ art.py
@@ -0,0 +1,35 @@
+logo="""                             ______________________________________
+                               |                                      |
+                    _.---------|.--.                                  |
+                 .-'  `       .'/  ``                                 |
+              .-'           .' |    /|                                |
+           .-'         |   /   `.__//                                 |
+        .-'           _.--/        /                                  |
+       |        _  .-'   /        /                                   |
+       |     ._  \      /     `  /                                    |
+       |        ` .    /     `  /                                     |
+       |         \ \ '/        /                                      |
+       |        - \  /        /|                                      |
+       |        '  .'        / |                                      |
+       |          '         |.'|                                      |
+       |                    |  |                                      |
+       |                    |  |______________________________________|
+       |                    |.'
+       |                    /
+       |                   /
+       |                  /
+       )                 /|
+    .A/`-.              / |
+   AMMMA. `-._         / /
+  AMMMMMMMMA. `-.     / /
+ AMMMMMMMMMMMMA. `.    /
+AMMMMMMMMMMMMMMMMA.`. /
+MMMMMMMMMMMMMMMMMMMA.`.
+MMMMMMMMMMMMMMMMMMMMMA.`.
+MMMMMMMMMMMMMMMMMMMMMMMA.
+MMMMMMMMMMMMMMMMMMMMMMMMMA.
+MMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMV'
+
+VK """
+print(logo)
